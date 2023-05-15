afd85af7efa471b5012f6a3841322ed5e72b42c4
Alan21103 <syahlansaputra03@gmail.com>
2023-05-15 11:32:11 +0700

    Membuat Class mahasiswa

--- mbrStatik/mbrStatik.cpp
+++ mbrStatik/mbrStatik.cpp
@@ -3,12 +3,3 @@
 using namespace std;
 
 class mahasiswa{
-public:
-	static int nim;
-	int id;
-	string nama;
-
-	void setID();
-	void printAll();
-	mahasiswa(string pnama):nama(pnama){setID();}
-};
